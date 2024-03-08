6d7e7433ef05ab342731c0286cbee0d34300286d
William Cute <williamjamesjanohan1997@gmail.com>
2024-03-08 18:59:52 +0800

    First commit

---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer(){ 
+    return(
+        <footer>
+            <p>&copy; 2024 IT-SYSARCH31-78437 Made by William James Janohan</p>
+        </footer>
+    );
+}
---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+function Header(){
+
+    return(
+        <header>
+            <h1>William James</h1>
+            <nav>
+                <ul>
+                    <li><a href="#"></a>Home</li>
+                    <li><a href="#"></a>About</li>
+                    <li><a href="#"></a>Service</li>
+                    <li><a href="#"></a>Contact</li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+
+}
+
+export default Header
